393c67482408ec7d3908d677992b5636a05b8a82
abdulbasit92786 <hassenahassena711@gmail.com>
2025-06-28 00:18:16 +0500

    Delete nft.js

--- nft.js
+++
@@ -1,6 +0,0 @@
-// This file will handle NFT logic like purchase, activate, etc.
-
-function activateNFT(planName) {
-  alert(`${planName} NFT Activated! 🎉`);
-  // In real version, connect to backend
-}
