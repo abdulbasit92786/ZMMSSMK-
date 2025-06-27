355971e80c1b5d9040645ea5fdf8cc10509e933f
abdulbasit92786 <hassenahassena711@gmail.com>
2025-06-28 00:17:00 +0500

    Delete referral.js

--- referral.js
+++
@@ -1,7 +0,0 @@
-function copyReferral() {
-  const input = document.getElementById("referralLink");
-  input.select();
-  input.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-  alert("Referral link copied! 📋");
-}
