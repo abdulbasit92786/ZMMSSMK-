7cccc72cfb1b70e5f329c5b52db4d91c3adc48cd
abdulbasit92786 <hassenahassena711@gmail.com>
2025-06-28 21:56:52 +0500

    Delete register.js

--- register.js
+++
@@ -1,6 +0,0 @@
-function sendVerificationCode() {
-  let email = document.getElementById('email').value;
-  // simulate code send
-  localStorage.setItem('register_email', email);
-  window.location.href = 'verify.html';
-}
