d8619e8d62ec2b0b9b76f3b21409482846de3758
abdulbasit92786 <hassenahassena711@gmail.com>
2025-07-25 00:27:09 +0500

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+// 🔐 Firebase configuration (Secret - Don't upload to GitHub)
+const firebaseConfig = {
+  apiKey: "AIzaSyCVcY3sYvcwxW4X4PnjS8OE-kHF3ODaHOM",
+  authDomain: "zmmssmk.firebaseapp.com",
+  databaseURL: "https://zmmssmk-default-rtdb.firebaseio.com",
+  projectId: "zmmssmk",
+  storageBucket: "zmmssmk.appspot.com",
+  messagingSenderId: "677420760492",
+  appId: "1:677420760492:web:2c27c0f0ed6490b8dfce09",
+  measurementId: "G-7J7BEDY8H1"
+};
+
+// 🌐 Export the config
+export default firebaseConfig;
